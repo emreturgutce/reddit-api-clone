3db6d439dcf99c28b9fbf1c4bb65e8d5e85bc6e4
emreturgutce <emrebursalee@gmail.com>
2020-10-14 11:51:40 +0300

    using not-found and error-handler middlewares

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
+import { errorHandler } from './middlewares/error-handler';
+import { notFound } from './middlewares/not-found';
 
 const app = express();
 
@@ -10,5 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
+
+app.use(notFound);
+app.use(errorHandler);
 
 export { app };
