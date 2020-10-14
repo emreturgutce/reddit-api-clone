43025005b9698361f04e8e79ca967f54cf123fa2
emreturgutce <emrebursalee@gmail.com>
2020-10-14 11:34:23 +0300

    colors added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import 'colors';
 
 console.clear();
 
@@ -8,5 +9,5 @@ import { app } from './app';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`App is running on port ${PORT}`);
+  console.log(`🚀 App is running on port ${PORT}`.green);
 });
