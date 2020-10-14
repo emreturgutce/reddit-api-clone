7e7c3ae3eae012d473613b20727f03c16fb76f84
emreturgutce <emrebursalee@gmail.com>
2020-10-15 01:05:06 +0300

    validating sign up

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,10 +1,21 @@
 import { Request, Response, NextFunction, Router } from 'express';
 import { passportSignup } from '../middlewares/passport-signup';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = Router();
 
-router.get(
+router.post(
   '/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('username').notEmpty().withMessage('Username must be provided'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  validateRequest,
   passportSignup,
   (request: Request, response: Response) => {
     response.status(201).json({
