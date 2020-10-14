21be3c311461f3f633200735df54942b5ffdf529
emreturgutce <emrebursalee@gmail.com>
2020-10-15 00:32:32 +0300

    using cookieSession mw

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,10 @@ import cors from 'cors';
 import morgan from 'morgan';
 import { errorHandler } from './middlewares/error-handler';
 import { notFound } from './middlewares/not-found';
+import cookieSession from 'cookie-session';
+import { passportSignup } from './middlewares/passport-signup';
+import { passportJwt } from './middlewares/passport-jwt';
+import { passportLogin } from './middlewares/passport-login';
 
 const app = express();
 
@@ -12,6 +16,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
+app.use(
+  cookieSession({
+    name: 'auth_token',
+    signed: false,
+  })
+);
 
 app.use(notFound);
 app.use(errorHandler);
