ad3794139e1b7b162bc5b534e1a3509e876406bd
emreturgutce <emrebursalee@gmail.com>
2020-10-15 14:30:42 +0300

    wrapping all test with Signup Route Handler Test Suite

--- src/routes/__tests__/signup.test.ts
+++ src/routes/__tests__/signup.test.ts
@@ -2,120 +2,122 @@ import request from 'supertest';
 import { app } from '../../app';
 import { User } from '../../models/user';
 
-it('Should return 400 Bad Request for invalid email address', async () => {
-  const response = await request(app)
-    .post('/signup')
-    .send({
-      username: 'test',
-      email: 'notvalidemail',
-      password: 'password',
-    })
-    .expect(400);
+describe('Signup Route Handler Test Suite', () => {
+  it('Should return 400 Bad Request for invalid email address', async () => {
+    const response = await request(app)
+      .post('/signup')
+      .send({
+        username: 'test',
+        email: 'notvalidemail',
+        password: 'password',
+      })
+      .expect(400);
 
-  expect(response.body.message).toEqual('Email must be valid');
-});
-
-it('Should return 400 Bad Request for invalid password', async () => {
-  const response = await request(app)
-    .post('/signup')
-    .send({
-      username: 'test',
-      email: 'test@test.com',
-      password: '',
-    })
-    .expect(400);
-
-  expect(response.body.message).toEqual(
-    'Password must be between 4 and 20 characters'
-  );
-});
-
-it('Should return 400 Bad Request for invalid username', async () => {
-  const response = await request(app)
-    .post('/signup')
-    .send({
-      username: '',
-      email: 'test@test.com',
-      password: 'password',
-    })
-    .expect(400);
-
-  expect(response.body.message).toEqual('Username must be provided');
-});
-
-it('Should return 400 Bad Request and proper message for invalid username, password', async () => {
-  const response = await request(app)
-    .post('/signup')
-    .send({
-      username: '',
-      email: 'test@test.com',
-      password: '',
-    })
-    .expect(400);
-
-  expect(response.body.message).toContain('Username must be provided');
-  expect(response.body.message).toContain(
-    'Password must be between 4 and 20 characters'
-  );
-});
-
-it('Should return 201 for valid inputs', async () => {
-  const response = await request(app)
-    .post('/signup')
-    .send({
-      username: 'test',
-      email: 'test@test.com',
-      password: 'test',
-    })
-    .expect(201);
-});
-
-it('Should return 400 for not unique email', async () => {
-  const email = 'test@test.com';
-
-  await request(app).post('/signup').send({
-    username: 'test',
-    email,
-    password: 'test',
+    expect(response.body.message).toEqual('Email must be valid');
   });
 
-  const response = await request(app)
-    .post('/signup')
-    .send({
-      username: 'test2',
+  it('Should return 400 Bad Request for invalid password', async () => {
+    const response = await request(app)
+      .post('/signup')
+      .send({
+        username: 'test',
+        email: 'test@test.com',
+        password: '',
+      })
+      .expect(400);
+
+    expect(response.body.message).toEqual(
+      'Password must be between 4 and 20 characters'
+    );
+  });
+
+  it('Should return 400 Bad Request for invalid username', async () => {
+    const response = await request(app)
+      .post('/signup')
+      .send({
+        username: '',
+        email: 'test@test.com',
+        password: 'password',
+      })
+      .expect(400);
+
+    expect(response.body.message).toEqual('Username must be provided');
+  });
+
+  it('Should return 400 Bad Request and proper message for invalid username, password', async () => {
+    const response = await request(app)
+      .post('/signup')
+      .send({
+        username: '',
+        email: 'test@test.com',
+        password: '',
+      })
+      .expect(400);
+
+    expect(response.body.message).toContain('Username must be provided');
+    expect(response.body.message).toContain(
+      'Password must be between 4 and 20 characters'
+    );
+  });
+
+  it('Should return 201 for valid inputs', async () => {
+    const response = await request(app)
+      .post('/signup')
+      .send({
+        username: 'test',
+        email: 'test@test.com',
+        password: 'test',
+      })
+      .expect(201);
+  });
+
+  it('Should return 400 for not unique email', async () => {
+    const email = 'test@test.com';
+
+    await request(app).post('/signup').send({
+      username: 'test',
       email,
       password: 'test',
-    })
-    .expect(400);
+    });
 
-  expect(response.body.message).toEqual(
-    'duplicate key value violates unique constraint'
-  );
-});
+    const response = await request(app)
+      .post('/signup')
+      .send({
+        username: 'test2',
+        email,
+        password: 'test',
+      })
+      .expect(400);
 
-it('Should be same values in database', async () => {
-  const username = 'test';
-  const email = 'test@example.com';
-
-  const response = await request(app).post('/signup').send({
-    username,
-    email,
-    password: 'test',
+    expect(response.body.message).toEqual(
+      'duplicate key value violates unique constraint'
+    );
   });
 
-  const user = await User.findOne(response.body.user.id);
+  it('Should be same values in database', async () => {
+    const username = 'test';
+    const email = 'test@example.com';
 
-  expect(user!.username).toEqual(username);
-  expect(user!.email).toEqual(email);
-});
+    const response = await request(app).post('/signup').send({
+      username,
+      email,
+      password: 'test',
+    });
 
-it('Should return cookie', async () => {
-  const response = await request(app).post('/signup').send({
-    username: 'test',
-    email: 'test@example.com',
-    password: 'test',
+    const user = await User.findOne(response.body.user.id);
+
+    expect(user!.username).toEqual(username);
+    expect(user!.email).toEqual(email);
   });
 
-  expect(response.get('Set-Cookie')[0]).toBeDefined();
-  expect(response.get('Set-Cookie')[0]).toMatch(/auth_token=.*/);
+  it('Should return cookie', async () => {
+    const response = await request(app).post('/signup').send({
+      username: 'test',
+      email: 'test@example.com',
+      password: 'test',
+    });
+
+    expect(response.get('Set-Cookie')[0]).toBeDefined();
+    expect(response.get('Set-Cookie')[0]).toMatch(/auth_token=.*/);
+  });
 });
