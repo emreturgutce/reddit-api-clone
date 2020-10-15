a1f284fd453d675d55393f2bbbb0ffe8e3bd752a
emreturgutce <emrebursalee@gmail.com>
2020-10-15 13:19:38 +0300

    changing database according to node environment

---
+++ ormconfig.js
@@ -0,0 +1,24 @@
+module.exports = {
+    type: 'postgres',
+    host: 'localhost',
+    port: '5432',
+    username: 'postgres',
+    password: 'postgres',
+    database: process.env.NODE_ENV === 'development' ?
+        'reddit_api_clone' :
+        process.env.NODE_ENV === 'test' ?
+        'reddit_api_clone_test' :
+        undefined,
+    synchronize: true,
+    logging: true,
+    entities: ['src/models/**/*.ts'],
+    migrations: ['src/migration/**/*.ts'],
+    subscribers: ['src/subscriber/**/*.ts'],
+    seeds: ['src/seeds/**/*.ts'],
+    factories: ['src/factories/**/*.ts'],
+    cli: {
+        entitiesDir: 'src/models',
+        migrationsDir: 'src/migration',
+        subscribersDir: 'src/subscriber',
+    },
+};
