d8b9a62717bba2b41858360f2d0ac08ceeaf6653
emreturgutce <emrebursalee@gmail.com>
2020-10-16 17:48:34 +0300

    test suite for logout route handler

---
+++ src/routes/__tests__/logout.test.ts
@@ -0,0 +1,20 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+describe('Logout Route Handler Test Suite', () => {
+  it('Should return 200 for successful logout', async () => {
+    await request(app)
+      .get('/logout')
+      .set('Cookie', await global.signup())
+      .send()
+      .expect(200);
+  });
+
+  it('Should return 400 for not authorized logout requests', async () => {
+    await request(app)
+      .get('/logout')
+      .set('Cookie', 'notavalidcookie')
+      .send()
+      .expect(401);
+  });
+});
