03b39f98194861b7635d76d6e51e220d6d97ed60
emreturgutce <emrebursalee@gmail.com>
2020-10-16 17:48:49 +0300

    create new subreddit route handler test suite implemented

---
+++ src/routes/subreddit/__tests__/create-new-subreddit.test.ts
@@ -0,0 +1,32 @@
+import request from 'supertest';
+import { app } from '../../../app';
+
+describe('Create New Subreddit Route Handler Test Suite', () => {
+  it('Should return 201 for invalid inputs', async () => {
+    const name = 'NodeJS';
+    const description = 'NodeJS subreddit description';
+
+    const response = await request(app)
+      .post('/r')
+      .set('Cookie', await global.signup())
+      .send({
+        name,
+        description,
+      })
+      .expect(201);
+
+    expect(response.body.subreddit.name).toEqual(name);
+    expect(response.body.subreddit.description).toEqual(description);
+  });
+
+  it('Should return 401 for unauthorized user', async () => {
+    await request(app)
+      .post('/r')
+      .set('Cookie', 'notavalidcookie')
+      .send({
+        name: 'NodeJS',
+        desription: 'NodeJS Subreddit description',
+      })
+      .expect(401);
+  });
+});
