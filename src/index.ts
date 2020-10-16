ea1f3bce8eada14ab8e810b30cbe55cab763f727
emreturgutce <emrebursalee@gmail.com>
2020-10-16 13:15:31 +0300

    deleting postgresql container after get sigint or sigterm signals

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import 'colors';
 import dotenv from 'dotenv';
+import { exec } from 'child_process';
 
 dotenv.config();
 
@@ -21,3 +22,18 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 App is running on port ${PORT}`.green);
 });
+
+function deletePostgresqlContainer() {
+  console.log(`🐘 Deleting PostgreSQL Container`.blue.bold);
+  exec('docker rm -f reddit-postgres-db', (err, stdout, stderr) => {
+    if (err) {
+      return console.log(`❌ Could not deleted PostgreSQL Container`.red.bold);
+    }
+
+    return console.log(`✔ PostgreSQL Container Deleted`.green.bold);
+  });
+}
+
+process.on('SIGINT', () => deletePostgresqlContainer());
+
+process.on('SIGTERM', () => deletePostgresqlContainer());
