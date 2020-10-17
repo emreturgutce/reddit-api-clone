0fd3dddf561e7a5badda616c5adc17f4e5e7cb74
emreturgutce <emrebursalee@gmail.com>
2020-10-17 14:30:19 +0300

    subreddit route

---
+++ src/routes/subreddit.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { getSubredditsRouteHandler } from '../controllers/subreddit/get-subreddits';
+import { validateCreateNewSubreddit } from '../middlewares/validate-create-new-subreddit';
+import { createNewSubredditRouteHandler } from '../controllers/subreddit/create-new-subreddit';
+import { joinSubredditRouteHandler } from '../controllers/subreddit/join-subreddit';
+import { postRouteHandler } from '../controllers/subreddit/post';
+import { validatePost } from '../middlewares/validate-post';
+
+const router = Router();
+
+router.get('/', getSubredditsRouteHandler);
+router.post('/', validateCreateNewSubreddit, createNewSubredditRouteHandler);
+router.get('/:subredditName/join', joinSubredditRouteHandler);
+router.post('/:subredditName', validatePost, postRouteHandler);
+
+export { router as subredditRouter };
