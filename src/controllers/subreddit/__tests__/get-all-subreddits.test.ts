3fe53a06252cf7a43cd5e6b64fe9b2f8b10e8f15
emreturgutce <emrebursalee@gmail.com>
2020-10-18 14:44:26 +0300

    get all subreddits test suite

---
+++ src/controllers/subreddit/__tests__/get-all-subreddits.test.ts
@@ -0,0 +1,20 @@
+import request from 'supertest';
+import { app } from '../../../app';
+
+describe('Get All Subreddits Route Handler Test Suite', () => {
+  it('Should return 200 for authorized user', async () => {
+    await request(app)
+      .get('/r/all')
+      .set('Cookie', await global.signup())
+      .send()
+      .expect(200);
+  });
+
+  it('Should return 401 for unauthorized user', async () => {
+    await request(app)
+      .get('/r/all')
+      .set('Cookie', 'notavalidcookie')
+      .send()
+      .expect(401);
+  });
+});
