9d6c345f1df3547c91f22abe0ca884cd620f5f67
emreturgutce <emrebursalee@gmail.com>
2020-10-18 13:47:40 +0300

    using get all subreddits route handler

--- src/routes/subreddit.ts
+++ src/routes/subreddit.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { getSubredditsRouteHandler } from '../controllers/subreddit/get-subreddits';
 import { validateCreateNewSubreddit } from '../middlewares/validate-create-new-subreddit';
 import { createNewSubredditRouteHandler } from '../controllers/subreddit/create-new-subreddit';
+import { getAllSubredditsRouteHandler } from '../controllers/subreddit/get-all-subreddits';
 import { joinSubredditRouteHandler } from '../controllers/subreddit/join-subreddit';
 import { postRouteHandler } from '../controllers/subreddit/post';
 import { validatePost } from '../middlewares/validate-post';
@@ -10,6 +11,7 @@ const router = Router();
 
 router.get('/', getSubredditsRouteHandler);
 router.post('/', validateCreateNewSubreddit, createNewSubredditRouteHandler);
+router.get('/all', getAllSubredditsRouteHandler);
 router.get('/:subredditName/join', joinSubredditRouteHandler);
 router.post('/:subredditName', validatePost, postRouteHandler);
 
