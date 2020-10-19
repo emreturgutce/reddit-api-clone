b275e7627af2b4b19c8b072ec2d0736c6743686d
emreturgutce <emrebursalee@gmail.com>
2020-10-19 14:49:48 +0300

    using profile avatar route handler

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import { homeRouteHandler } from '../controllers/index/home';
 import { loginRouteHandler } from '../controllers/index/login';
 import { logoutRouteHandler } from '../controllers/index/logout';
+import { profileAvatarRouteHandler } from '../controllers/index/profile-avatar';
 import { signupRouteHandler } from '../controllers/index/signup';
 import { passportJwt } from '../middlewares/passport-jwt';
 import { passportLogin } from '../middlewares/passport-login';
 import { passportSignup } from '../middlewares/passport-signup';
+import { uploadAvatar } from '../middlewares/upload-avatar';
 import { validateLogin } from '../middlewares/validate-login';
 import { validateSignup } from '../middlewares/validate-signup';
 
@@ -15,5 +17,11 @@ router.get('/', passportJwt, homeRouteHandler);
 router.post('/signup', validateSignup, passportSignup, signupRouteHandler);
 router.post('/login', validateLogin, passportLogin, loginRouteHandler);
 router.get('/logout', passportJwt, logoutRouteHandler);
+router.post(
+  '/profile/avatar',
+  passportJwt,
+  uploadAvatar,
+  profileAvatarRouteHandler,
+);
 
 export { router as indexRouter };
