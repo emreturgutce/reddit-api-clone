2ad785d8a43f5537bdc62c4b26a9a04f6feaf221
emreturgutce <emrebursalee@gmail.com>
2020-10-21 17:06:49 +0300

    redis config file

---
+++ src/config/redis.ts
@@ -0,0 +1,46 @@
+import { RedisClient } from 'redis';
+import { promisify } from 'util';
+
+interface RedisActions {
+  sadd: (key: string, val: string) => any;
+  smembers: (key: string) => Promise<string[]>;
+}
+
+class Redis {
+  readonly client: RedisClient;
+
+  readonly actions: RedisActions;
+
+  constructor() {
+    this.client = new RedisClient({});
+    this.onConnect();
+    this.onError();
+    this.actions = this.initializeActions();
+  }
+
+  private initializeActions(): RedisActions {
+    return {
+      smembers: (key) => {
+        const smembersAsync = promisify(this.client.smembers).bind(this.client);
+        return smembersAsync(key);
+      },
+      sadd: (key, val) => this.client.sadd(key, val),
+    };
+  }
+
+  private onConnect() {
+    return this.client.on('connect', () => {
+      console.log('👹 Connected to Redis'.red.bold);
+    });
+  }
+
+  private onError() {
+    return this.client.on('error', (err) => {
+      console.error(err);
+    });
+  }
+}
+
+const redis = new Redis();
+
+export { redis };
