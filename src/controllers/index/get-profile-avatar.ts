cc81bde9ca9483bf1338a90982a939c9702539fd
emreturgutce <emrebursalee@gmail.com>
2020-10-21 19:19:09 +0300

    unused variables deleted

--- src/controllers/index/get-profile-avatar.ts
+++ src/controllers/index/get-profile-avatar.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
-import fs from 'fs';
 import { User } from '../../models/user';
-import { decrypt } from '../../utils/encrypt-decrypt';
 
 export const getProfileAvatarRouteHandler = async (
   request: Request,
