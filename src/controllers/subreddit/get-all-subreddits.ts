e08ce32665b42550be03d30133e911d14f5e2dbf
emreturgutce <emrebursalee@gmail.com>
2020-10-21 17:07:58 +0300

    getting subreddits from cache

--- src/controllers/subreddit/get-all-subreddits.ts
+++ src/controllers/subreddit/get-all-subreddits.ts
@@ -1,14 +1,36 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import { Subreddit } from '../../models/subreddit';
+import { redis } from '../../config/redis';
 
 export const getAllSubredditsRouteHandler = async (
   request: Request,
   response: Response,
 ) => {
-  const subreddits = await getRepository(Subreddit).find({
-    select: ['id', 'name', 'description'],
-  });
+  let subreddits: Subreddit[] | string[] = await redis.actions.smembers(
+    'subreddits',
+  );
+
+  if (subreddits.length === 0) {
+    const srs = await getRepository(Subreddit).find({
+      select: ['id', 'name', 'description'],
+    });
+
+    srs.forEach((s: Subreddit) => {
+      redis.actions.sadd(
+        'subreddits',
+        JSON.stringify({
+          id: s.id,
+          name: s.name,
+          description: s.description,
+        }),
+      );
+    });
+
+    subreddits = srs;
+  } else {
+    subreddits = subreddits.map((s) => JSON.parse(s));
+  }
 
   response.json({ subreddits });
 };
