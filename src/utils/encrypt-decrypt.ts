7e5cef32f08e49f5c345a0935a65e330c3e7bf07
emreturgutce <emrebursalee@gmail.com>
2020-10-22 18:26:41 +0300

    new encrytion and decryption method

--- src/utils/encrypt-decrypt.ts
+++ src/utils/encrypt-decrypt.ts
@@ -1,11 +1,31 @@
-import CryptoJS from 'crypto-js';
+import crypto from 'crypto';
 
-export const encrypt = (text: string) => {
-  return CryptoJS.AES.encrypt(text, process.env.ENCRYPTION_KEY!).toString();
+const algorithm = process.env.ENCRYPTION_ALGORITHM!;
+
+const key = crypto
+  .createHash('sha256')
+  .update(String(process.env.ENCRYPTION_KEY || 'MySuperSecretKey'))
+  .digest('base64')
+  .substr(0, 32);
+
+export const encrypt = (buffer: Buffer) => {
+  const iv = crypto.randomBytes(16);
+
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+  const result = Buffer.concat([iv, cipher.update(buffer), cipher.final()]);
+
+  return result;
 };
 
-export const decrypt = (encrypted: string) => {
-  return CryptoJS.AES.decrypt(encrypted, process.env.ENCRYPTION_KEY!).toString(
-    CryptoJS.enc.Utf8,
-  );
+export const decrypt = (encrypted: Buffer) => {
+  const iv = encrypted.slice(0, 16);
+
+  const e = encrypted.slice(16);
+
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+  const result = Buffer.concat([decipher.update(e), decipher.final()]);
+
+  return result;
 };
