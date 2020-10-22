7147c1f52dfd06cde3c92eb3dbb6cef3c3490acb
emreturgutce <emrebursalee@gmail.com>
2020-10-22 18:27:17 +0300

    decrypting image after getting from db

--- src/controllers/index/get-profile-avatar.ts
+++ src/controllers/index/get-profile-avatar.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import fs from 'fs';
 import { User } from '../../models/user';
+import { decrypt } from '../../utils/encrypt-decrypt';
 
 export const getProfileAvatarRouteHandler = async (
   request: Request,
@@ -11,6 +13,15 @@ export const getProfileAvatarRouteHandler = async (
   const user = await getRepository(User).findOneOrFail(userId, {
     select: ['avatar'],
   });
+
+  let avatar: Buffer | undefined;
+
+  if (user.avatar) {
+    const decrypted = decrypt(user.avatar);
+    fs.writeFileSync('second.txt', decrypted);
+    fs.writeFileSync('picture.png', decrypted);
+    avatar = decrypted;
+  }
 
   response.json({ message: 'Avatar image saved', avatar: user.avatar });
 };
