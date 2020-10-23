0eeb63e6499d55d5fdaed832339c629875ad7b07
emreturgutce <emrebursalee@gmail.com>
2020-10-23 16:54:57 +0300

    using express-rate-limit

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,18 @@ import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
 import cookieSession from 'cookie-session';
+import rateLimit from 'express-rate-limit';
 import { router } from './routes';
 
 const app = express();
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 100,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
